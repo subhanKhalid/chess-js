3a09311ed29afa654e30390816ae41900b084201
Subhan Khalid <subhannkhalid@gmail.com>
2023-12-17 21:18:26 -0500

    chore: added some docs

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// app.js
+// app.js main setup and chess logic
 const gameBoard = document.querySelector("#gameboard");
 const playerDisplay = document.querySelector("#player");
 const infoDisplay = document.querySelector("#info-display");
